b5692783c4922d339e356d1d4f01035196888399
yabreu65 <yoryiabreu@gmail.com>
2023-04-12 14:09:01 -0300

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-main>
+      <Header />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import Header from "@/components/Header.vue";
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="d-flex align-center text-center fill-height">
+      <v-row class="d-flex align-center justify-center">
+        <v-col cols="auto"> <h1>ggggggcd</h1></v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+//
+</script>
